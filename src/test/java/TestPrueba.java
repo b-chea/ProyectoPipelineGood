c719bea6234e86146cf174bea60b531eb4a96948
Ivan <ivef.8990@gmail.com>
2025-02-04 11:18:29 -0500

    first commit

---
+++ src/test/java/TestPrueba.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestPrueba {
+    private WebDriver firefoxDriver;
+    @Test
+    public void hacer_una_busqueda(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //Se define tiempo de espera
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Abrimos una URL
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+
+        //Encontrar e interactuar con elementos
+        driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("Cantidad de paises en América");
+        driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys(Keys.ENTER);
+
+        driver.findElement(By.xpath("//h3[contains(text(),'Mapa Político de América: Países y Capitales - Web')]")).click();
+        driver.quit();
+    }
+}
